a7006832aaeb8b85a0b9adcb498a6424d99a1de0
MamaneHassane <hassane.mamane017@gmail.com>
2024-07-15 16:55:02 +0100

    building

--- src/pages/CurriculumVitae.tsx
+++ src/pages/CurriculumVitae.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 // @ts-ignore
 import {fields, languages} from "../constants/constants.ts";
 import {useState} from "react";
+import {Link} from "react-router-dom";
+import {cilArrowLeft, cilHome} from "@coreui/icons";
+import CIcon from "@coreui/icons-react";
 
 const CurriculumVitae = () => {
     // The langage you want to see the CV
@@ -17,11 +20,21 @@ const CurriculumVitae = () => {
         setField(event.target.value);
     }
     return (
-        <div>
+        <div className={"overflow-hidden"}>
+            <button
+                className="flex flex-row p-2 mt-1 mx-2 justify-center items-center hover:bg-white hover:text-red-500 bg-red-500 text-white rounded-lg font-bold"
+            >
+                <Link
+                    className="flex flex-row items-center"
+                    to="/"
+                >
+                    <CIcon icon={cilArrowLeft} className="h-4 mx-1 text-blue" />
+                </Link>
+            </button>
             <div className={"flex flex-row gap-2 p-2 w-full"}>
                 <select
                     name={language}
-                    onChange={(event)=>handleLanguageChange(event)}
+                    onChange={(event) => handleLanguageChange(event)}
                     className={"w-1/2 p-2 rounded-md bg-pink-500 text-white"}
                 >
                     <option
@@ -38,13 +51,13 @@ const CurriculumVitae = () => {
 
                 <select
                     name={field}
-                    onChange={(event)=>handleFieldChange(event)}
+                    onChange={(event) => handleFieldChange(event)}
                     className={
                         field === fields.spr_rct ?
-                        "w-1/2 p-2 rounded-md bg-green-500 text-white"
-                        :
-                        "w-1/2 p-2 rounded-md bg-indigo-500 text-white"
-                }>
+                            "w-1/2 p-2 rounded-md bg-green-500 text-white"
+                            :
+                            "w-1/2 p-2 rounded-md bg-indigo-500 text-white"
+                    }>
                     <option
                         value={fields.dnt_ang}
                     >
